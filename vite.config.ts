781f7d033b333dba7a5047fbf52b6f364d254811
akgupta <akgupta@mafcarrefour.com>
2026-03-10 17:51:22 +0530

    Minor changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
@@ -16,5 +16,8 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
+  },
+  preview: {
+    allowedHosts: ['pnp.mafcarrefour.local'],
   },
 });
